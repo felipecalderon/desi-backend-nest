a010f3e7e93c631967b503488aa749fcfe4b38aa
mikeagf300 <mikeagf300@gmail.com>
2025-10-21 15:45:16 -0600

    Se creo modulo stores con estructura CRUD, se trabajara para adapatar stores del notion al backend actual

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './datasource/database.module';
+import { StoresModule } from './stores/stores.module';
 
 @Module({
-  imports: [DatabaseModule, UsersModule],
+  imports: [DatabaseModule, UsersModule, StoresModule],
 })
 export class AppModule {}
